0bde00e55d32b394966fb44786abfcb773dce1d0
Moula imen <imenmoula8@gmail.com>
2024-01-21 17:08:51 +0100

    fix annee

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="footer">
     <div class="copyright">
-        <p>Copyright © Designed &amp; Developed by <a href="http://dexignzone.com/" target="_blank">DexignZone</a> 2020</p>
+        <p>Copyright © Designed &amp; Developed by <a href="http://dexignzone.com/" target="_blank">DexignZone</a> 2024</p>
     </div>
 </div>	
